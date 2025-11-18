12f2ba16c6772039ce9365fdb87c84be8ae098d7
Rodrigo Arturo Gutierrez Barrera <rodrigogutierrez@MacBook-PRO-de-Rodrigo.local>
2025-11-18 17:26:09 -0600

    add js animation

---
+++ src/js/navigation.js
@@ -0,0 +1,35 @@
+
+
+const menuButton=document.querySelector('#menu-button')
+const navMenu=document.querySelector('#nav-menu')
+const liMenu=document.querySelectorAll('#nav-menu li')
+
+menuButton.addEventListener('click',()=>{
+    navMenu.classList.remove('hidden','animate-slide-out')
+   navMenu.classList.add('flex','animate-slide-in')
+  
+
+})
+
+liMenu.forEach((liElement)=>{
+   liElement.addEventListener('click',()=>{
+     if(window.innerWidth >= 640) return;
+        navMenu.classList.add('animate-slide-out')
+        navMenu.classList.remove('animate-slide-in')
+       
+    
+   })
+})
+navMenu.addEventListener('animationend',(e)=>{
+    if(e.animationName !== 'slide-out')return;
+    navMenu.classList.add('hidden')
+    navMenu.classList.remove('flex')
+
+    
+})
+
+window.addEventListener('resize',()=>{
+    if(window.innerWidth >= 640){
+        navMenu.classList.remove('animate-slide-in','animate-slide-out')
+    }
+})

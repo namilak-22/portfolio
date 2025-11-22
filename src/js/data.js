57335be0281d42a5ae6d5af9a3f314947855e2e8
Rodrigo Gutiérrez <namilak.22@gmail.com>
2025-11-22 15:34:01 -0600

    proyecto agregado

--- src/js/data.js
+++ src/js/data.js
@@ -31,5 +31,17 @@ export const projects=[
      tech:'REACT, VITE, TYPESCRIPT, TAILWINDCSS,EXPRESS JS',
      link:'https://github.com/namilak-22/rest_apis_typescript_frontend'   
     
+    },
+    {title:'Control de Gastos',
+     img:{
+        src:'./img/control_gastos.png',
+        alt:'Portfolio Rodrigo'
+     },
+     description:'Administra Presupuesto de gastos, con categorías y diseño gráfico',
+     titleTech:'⚙️ Tecnologías:',
+     tech:'REACT, VITE, TYPESCRIPT, TAILWINDCSS',
+     link:'https://github.com/namilak-22/controlGastos/tree/main'   
+    
     }
+    
 ]

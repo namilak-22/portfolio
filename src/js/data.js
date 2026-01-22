f94dafb69a565fcbfd3ac022131bab9263c4338b
Rodrigo Gutiérrez <namilak.22@gmail.com>
2026-01-22 15:11:02 -0600

    proyecto uptask agregado

--- src/js/data.js
+++ src/js/data.js
@@ -1,44 +1,58 @@
-export const projects=[
-    {title:'Formulario de Pago OpenPay Unini',
-     img:{
-        src:'./img/unini.png',
-        alt:'Proyecto Unini'
-     },
-     description:'Un carrusel de pago con protocolo de openpay bbva, con 3d secure para corroborar tarjetas de crédito o débito legítimas.',
-     tech:'PHP, HTML, CSS, JAVASCRIPT',
-     link:'https://github.com/namilak-22/uniniPagoOpenpay/tree/master'   
-    
+export const projects = [
+  {
+    title: "Formulario de Pago OpenPay Unini",
+    img: {
+      src: "./img/unini.png",
+      alt: "Proyecto Unini",
     },
-    {title:'Portfolio',
-     img:{
-        src:'./img/portfolio-project.png',
-        alt:'Portfolio Rodrigo'
-     },
-     description:'Página Realizada para que conozcan sobre mi trayectoria y gustos',
-     tech:'HTML5,TAILWINDCSS,JAVASCRIPT',
-     link:'https://github.com/namilak-22/portfolio'   
-    
+    description:
+      "Un carrusel de pago con protocolo de openpay bbva, con 3d secure para corroborar tarjetas de crédito o débito legítimas.",
+    tech: "PHP, HTML, CSS, JAVASCRIPT",
+    link: "https://github.com/namilak-22/uniniPagoOpenpay/tree/master",
+  },
+  {
+    title: "Portfolio",
+    img: {
+      src: "./img/portfolio-project.png",
+      alt: "Portfolio Rodrigo",
     },
-    {title:'Administrador Inventarios',
-     img:{
-        src:'./img/admon-projects.png',
-        alt:'Portfolio Rodrigo'
-     },
-     description:'Administra inventarios, alta y baja de productos',
-     tech:'REACT, VITE, TYPESCRIPT, TAILWINDCSS,EXPRESS JS',
-     link:'https://github.com/namilak-22/rest_apis_typescript_frontend'   
-    
+    description:
+      "Página Realizada para que conozcan sobre mi trayectoria y gustos",
+    tech: "HTML5,TAILWINDCSS,JAVASCRIPT",
+    link: "https://github.com/namilak-22/portfolio",
+  },
+  {
+    title: "Administrador Inventarios",
+    img: {
+      src: "./img/admon-projects.png",
+      alt: "Portfolio Rodrigo",
     },
-    {title:'Control de Gastos',
-     img:{
-        src:'./img/control_gastos.png',
-        alt:'Portfolio Rodrigo'
-     },
-     description:'Administra Presupuesto de gastos, con categorías y diseño gráfico',
-     titleTech:'⚙️ Tecnologías:',
-     tech:'REACT, VITE, TYPESCRIPT, TAILWINDCSS',
-     link:'https://github.com/namilak-22/controlGastos/tree/main'   
-    
-    }
-    
-]
+    description: "Administra inventarios, alta y baja de productos",
+    tech: "REACT, VITE, TYPESCRIPT, TAILWINDCSS,EXPRESS JS",
+    link: "https://github.com/namilak-22/rest_apis_typescript_frontend",
+  },
+  {
+    title: "Control de Gastos",
+    img: {
+      src: "./img/control_gastos.png",
+      alt: "Portfolio Rodrigo",
+    },
+    description:
+      "Administra Presupuesto de gastos, con categorías y diseño gráfico",
+    titleTech: "⚙️ Tecnologías:",
+    tech: "REACT, VITE, TYPESCRIPT, TAILWINDCSS",
+    link: "https://github.com/namilak-22/controlGastos/tree/main",
+  },
+  {
+    title: "Uptask",
+    img: {
+      src: "./img/uptask.png",
+      alt: "Portfolio Rodrigo",
+    },
+    description:
+      "Web Service que administra proyectos, asigna tareas y controla colaboradores de un proyecto para su realización",
+    titleTech: "⚙️ Tecnologías:",
+    tech: "REACT, VITE, TYPESCRIPT, TAILWINDCSS,EXPRESS,MONGODB,NODEJS",
+    link: "https://github.com/namilak-22/Uptask_Frontend",
+  },
+];
